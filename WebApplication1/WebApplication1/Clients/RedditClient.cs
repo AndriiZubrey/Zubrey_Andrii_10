eceec48e6dd3015a856787c9be4e7a6d46694a81
AndriiZubrey <76507660+AndriiZubrey@users.noreply.github.com>
2021-05-12 02:42:47 +0300

    Add files via upload

---
+++ WebApplication1/WebApplication1/Clients/RedditClient.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Clients
+{
+    public class RedditClient
+    {
+        private HttpClient _client;
+        private static string _address;
+        private static string _apikey;
+
+        public RedditClient()
+        {
+            _address = Constants.address;
+            _apikey = Constants.apikey;
+
+            _client = new HttpClient();
+            _client.BaseAddress = Uri(_address);
+            //_client.DefaultRequestHeaders.
+        }
+
+        public async Task GetTopSR()
+        {
+            var responce = await _client.GetAsync();
+            responce.EnsureSuccessStatusCode();
+            var content = response.Content.ReadAsStringAsync().Result;
+            var result = JsonContent.
+        }
+    }
+}
